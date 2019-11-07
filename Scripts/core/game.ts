7c72951e86a28b801ff13d3f3547a144990aab6a
George Zhou <georgezhou89@hotmail.com>
2019-11-07 17:27:46 -0500

    Added Label and Button

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,44 +1,56 @@
-"use strict";
 // Immediate Invoked Anonymous Function
-(function () {
+
+(function() {
+
     // Global Game Variables
-    var canvas = document.getElementById("canvas");
-    var stage;
-    var helloLabel;
-    var newgameButton;
+    let canvas = document.getElementById("canvas");
+    let stage:createjs.Stage;
+    let helloLabel:objects.Label; 
+    let newgameButton:objects.Button;
+
     function Init() {
         console.log("Initialization Start");
         Start();
     }
+
     function Start() {
         console.log("Starting Application...");
+
         // Initialize CreateJS
         stage = new createjs.Stage(canvas);
         // Freqeuncy of checks. Computationally expensive. Turn on in menus, Turn off in game
-        stage.enableMouseOver(20);
+        stage.enableMouseOver(20); 
         createjs.Ticker.framerate = 60; // 60 FPS
         createjs.Ticker.on("tick", Update);
         Main();
     }
+
     function Update() {
         stage.update();
         helloLabel.rotation += 5;
     }
-    function clickableButtonMouseClick() {
+
+    function clickableButtonMouseClick():void {
         helloLabel.text = "Clicked";
         console.log("AHHHHHHH");
     }
+
     function Main() {
         console.log("Game Start...");
+
         helloLabel = new objects.Label("Hello World", "40px", "Consolas", "#000000", 320, 240, true);
+
         stage.addChild(helloLabel); // Add the label to the stage
+
         // Button Initialization
         newgameButton = new objects.Button("./Assets/NewGameButton.png", 320, 340);
+
         newgameButton.regX = 95;
         newgameButton.regY = 24.5;
+
         newgameButton.on("click", clickableButtonMouseClick);
         stage.addChild(newgameButton);
     }
+
     window.onload = Init;
 })();
-//# sourceMappingURL=game.js.map
---
+++ Scripts/objects/CommomUIElement.ts
@@ -0,0 +1,45 @@
+module objects {
+    // Buttons
+    export class Button extends createjs.Bitmap {
+        // Variables
+        // Constructor
+        constructor(imagePath: string, x: number = 0, y: number = 0) {
+            super(imagePath);
+
+            // Set default position
+            this.x = x;
+            this.y = y;
+
+            // Set event handlers
+            this.on("mouseover", this.mouseOver);
+            this.on("mouseout", this.mouseOut);
+        }
+
+        // Methods
+        // Mouseover Effects
+        private mouseOver(): void {
+            this.alpha = 0.7;
+        }
+
+        private mouseOut(): void {
+            this.alpha = 1.0;
+        }
+    }
+    // Labels
+    export class Label extends createjs.Text {
+        constructor(labelString: string, fontSize: string, fontFamily: string,
+            fontColor: string, x: number = 0, y: number = 0, isCentered: boolean = false) {
+            super(labelString, fontSize + " " + fontFamily, fontColor);
+
+            // Set the registration point if true to be in the middle
+            if (isCentered) {
+                this.regX = this.getMeasuredWidth() * 0.5;
+                this.regY = this.getMeasuredHeight() * 0.5;
+            }
+
+            // Set default position
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
